9731b31f3890cc7883c9dacb8318844e5360bec9
Alexander Ledokhovich <alexander.ledokhich@mail.ru>
2026-04-21 23:01:44 +0300

    add: Write a Pydantic schemas(data validation)

---
+++ app/schemas.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class UserOut(BaseModel):
+    id:int
+    username:str
+    email:str
+    created_at:datetime
+    
+    class Config:
+        from_attributes=True
+
+class NoteCreate(BaseModel):
+    title:str
+    content: Optional[str] = None
+
+class NoteOut(BaseModel):
+    id:int
+    title:str
+    content:Optional[str]=None
+    created_at:datetime
+    updated_at:datetime
+    user_id:int
+
+    class Config:
+        from_attributes=True
+
+class Token(BaseModel):
+    access_token:str
+    token_type:str

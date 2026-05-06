5a2708b1386e7b3b1750f5d9cb9195daa46c8a79
Alexander Ledokhovich <alexander.ledokhich@mail.ru>
2026-05-07 00:00:44 +0300

    Full-fledged completion of the project

--- app/auth.py
+++ app/auth.py
@@ -14,7 +14,7 @@ ALGORITHM="HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context=CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme=OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
